b0cd8b656a309d98a02a1f210b4997c9ed4288f2
김태윤 <kim0517@sju.ac.kr>
2021-07-06 12:14:51 +0900

    Router and Routing Table1

--- Lec3.js
+++ Lec3.js
@@ -1,19 +1,42 @@
-const rNewLine =/[\r\n\l]/g;
-const rQuat = /"/g;
+const stringCheck ={
+    data:[[/[\r\n\l]/g],[/"/g,"\\\""],[/\t/g, "\\t"]],
+    convert(v){
+        return this.data.reduce((acc,curr)=>acc.replace(curr[0],curr[1]),v)
+    }
+};
 const el={
-    boolean:v=>v.toString(),
-    number:v=>v.toString(),
-    string:v=>`"${v}"`,
-    stringify(v){
-        return this[typeof v]?.(v) ?? "null";
+    data:{
+        boolean:v=>v.toString(),
+        number:v=>v.toString(),
+        string:v=>`"${stringCheck.convert(v)}"`
+    },
+    stringify(v){//라우터
+        return this.data[typeof v]?.(v) ?? "null";
     }
 }
 
 const recursive=(arr,acc,i)=>i<arr.length ?
     recursive(arr,acc+`,${el.stringify(arr[i])}`,i+1) :
     `[${acc.substr(1)}]`;
-const EMPTY={};
 const stringify=arr=>{
     if(!Array.isArray(arr)) throw "invalid arr";
     return arr.length===0 ? "[]": recursive(arr,"",0);
 }
+const EMPTY={};
+const forStringify=arr=>{
+    if(!Array.isArray(arr)) throw "invalid arr";
+    let result=EMPTY;
+    if(arr.length===0) result="[]";
+    else{
+        let acc="",i=0;
+        while(i<arr.length){
+            acc=acc+`,${el.stringify(arr[i])}`;
+            i=i+1;
+        }
+        result=`[${acc.substr(1)}]`;
+    }
+    if(result===EMPTY) throw "no processed" //shield
+    return result;
+}
+
+console.log(typeof stringify(["gdg",123,235]));
