c66760077566fb6ab582e955909728075fec88f9
김태윤 <kim0517@sju.ac.kr>
2021-07-13 12:36:17 +0900

    테이블 작성 및 OCP

--- Lec4.js
+++ Lec4.js
@@ -3,17 +3,24 @@ const arrToString=arr=>{
     for(const v of acc) accStr+=","+v;
     return "{"+accStr.substr(1)+"}";
 }
-
+const table={
+    array:(currEl,arr,acc,i,stack)=>{ // 전략 객체1
+        stack.push([arr,acc,i+1]);
+        return [arr[i],[],0];
+    },
+    number:(currEl,arr,acc,i,stack)=>{ // 전략 객체2
+        acc.push(""+v);
+        return [arr,acc,i+1];
+    }
+}
+const elementProcess=(currEl,arr,acc,i,stack)=>{
+    for(const v of table)v(currEl,arr,acc,i,stack)
+};
 const recursive=(arr,acc,i,stack)=>{
     if(i<arr.length){
         const currEl=arr[i];
-        if(Array.isArray(currEl)){
-            stack.push([arr,acc,i+1]);
-            return recursive(arr[i],[],0,stack);
-        }else{
-            acc.push(""+arr[i]);
-            return recursive(arr,acc,i+1,stack);
-        }
+        const [resultArr,resultAcc,resultIndex]=elementProcess(currEl,arr,acc,i,stack);
+        return recursive(resultArr,resultAcc,resultIndex,stack);
     }else{
         const accStr=arrToString(acc);
         const prev=stack.pop();
