ede8f86a7c4484b87e049bd4ba666a60f831e63c
TaeYoon17 <46375289+TaeYoon17@users.noreply.github.com>
2021-07-27 00:35:59 +0900

    Update Lec5.js

--- Lec5.js
+++ Lec5.js
@@ -19,58 +19,57 @@ if(1){
         return done ? join(acc) : recursive(iter,{pre:acc,k,v});
     };
     const stringify=obj=>recursive(objEntries(obj),null);
-    console.log(stringify(obj));
+    //console.log(stringify(obj));
 }
+
 const objEntries=function*(obj){
     for(const k in obj) if(obj.hasOwnProperty(k)) yield [k,obj[k]];
 };
-
-
 const obj={a:1,b:2,c:3,d:"absdf"};
 const convert=v=>""+v;
-const arrToString=acc=>{
+const arrToString=(acc,isObject)=>{
     let START,END;
-    if(acc.isObject) {
+    if(isObject) {
         START="{";END="}";
     }else{
         START="[";END="]";
     }
     let result="";
-    if(acc.prev){
-        let curr=acc;
+    let curr=acc;
     do{
-        result=","+(curr.isObject ? `"${curr.value[0]}":${convert(curr.value[1])}` : convert(curr.value)) + result;
+        result=","+(isObject ? `"${curr.value[0]}":${convert(curr.value[1])}` : convert(curr.value)) + result;
     }while(curr=curr.prev);
     result=result.substr(1);
-    }
     return START+result+END;
 }
 const _recursion=(iter,isObject,acc,prev)=>{
     const {done,value}=iter.next();
     if(!done){
         const v=isObject? value[1]:value; //내가 지금 무엇인지
+        console.log(v, isObject);
         switch(true){//내 자식이 무엇인지
             case Array.isArray(v):
-                return _recursion(v[Symbol.iterator](),false,null,{target:iter,isObject,accumulation:acc,prev:prev});
+                return _recursion(v[Symbol.iterator](),false,null,{target:iter,isObject,accumulation:acc,prev:prev,k:isObject ? value[0]:""});
             case v && typeof v=="object":
-                return _recursion(objEntries(v),true,null,{target:iter,accumulation:acc,prev:prev});
+                return _recursion(objEntries(v),true,null,{target:iter,isObject,accumulation:acc,prev:prev,k:isObject ? value[0]:""});
             default:
                 return _recursion(iter,isObject,{prev:acc,value},prev);
         }
     }
     else{
-        let accStr=accToString(acc);
+        let accStr=arrToString(acc,isObject);
+        console.log("prev",prev);
         if(prev){
-            return _recursion(prev.target,{prev:prev.accumulation,value:accStr},prev.prev);
+            return _recursion(prev.target,prev.isObject,{prev:prev.accumulation,value:prev.isObject? [prev.k,accStr]:accStr},prev.prev);
         }else{
             return accStr;
         }
     }
 }
 
-const stringify=obj=>_recursion(Array.isArray(v)? v[Symbol.iterator]():objEntries(v),!Array.isArray(v),null);
+const stringify=obj=>_recursion(Array.isArray(obj)? obj[Symbol.iterator]() : objEntries(obj),!Array.isArray(obj),null,null);
 //console.log([1,2,3,4,5][Symbol.iterator]());
-console.log(stringify([1,2,3,4,5][Symbol.iterator]()));
+console.log(stringify({a:1,b:2,c:"hello",d:[{a:1}]}));
 
 
 //과제1
